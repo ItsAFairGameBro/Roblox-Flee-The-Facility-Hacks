4cb94263764b22db55dca3f48e2c88b502e379fc
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-03-14 16:16:42 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1549,7 +1549,7 @@ local function LocalClubScriptFunction(Original_LocalClubScript)
 			if result and v149 and v149:IsA("BasePart") then
 				local v151 = v149.Transparency
 				v174 = 0.95
-				if not v149.CanCollide or (v149.Parent and v149.Parent:FindFirstChild("Humanoid")) then
+				if not v149.CanCollide or (v149.Parent and (v149.Parent:FindFirstChild("Humanoid") or v149.Parent.Parent:FindFirstChild("Humanoid"))) then
 					table.insert(v146, v149)
 					v137 = true
 					local v158 = PS:GetPlayerFromCharacter(v149.Parent)
@@ -1567,8 +1567,6 @@ local function LocalClubScriptFunction(Original_LocalClubScript)
 						else
 							return
 						end
-					else
-						return
 					end
 				else
 					return

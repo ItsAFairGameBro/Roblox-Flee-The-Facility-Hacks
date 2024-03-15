82b1f0cf507d6714c99ad15ea50a1ffe5e215807
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-03-14 22:33:00 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -4993,12 +4993,12 @@ AvailableHacks ={
 						local closestExitArea,dist=findClosestObj(getExitDoors(),(char.PrimaryPart and char.PrimaryPart.Position or newVector3()),3000,1)
 						while canRun() and closestExitArea~=nil and not closestExitArea:GetAttribute("Unreachable"..saveIndex) and not TSM.Escaped.Value do
 							local exitDoor = closestExitArea.Parent
-							if exitDoor:FindFirstChild("ExitDoorTrigger") and exitDoor.ExitDoorTrigger.ActionSign.Value == 10 then
+							if exitDoor:FindFirstChild("ExitDoorTrigger") and exitDoor.ExitDoorTrigger.ActionSign.Value == 10 and AvailableHacks.Blatant[15].DoorFuncts[exitDoor] then
 								AvailableHacks.Blatant[15].DoorFuncts[exitDoor]()
 							end
 							local exitDoorTrigger = closestExitArea.Parent.ExitDoorTrigger
 							local didReach=AvailableHacks.Bot[15].WalkPath(currentPath,closestExitArea,canRun)
-							while ((table.find(workspace:GetPartsInPart(char.HumanoidRootPart),closestExitArea)) and (not TSM.Escaped.Value)) do
+							while ((table.find(workspace:GetPartsInPart(char.HumanoidRootPart),closestExitArea)) and (not TSM.Escaped.Value) and exitDoor.ExitDoorTrigger.ActionSign.Value ~= 10) do
 
 								if human.FloorMaterial~=Enum.Material.Air then
 									human:ChangeState(Enum.HumanoidStateType.Jumping)

1fd424c9adeae58782cabb5eb49e0e420ba80ca5
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-03-19 19:51:36 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -4258,7 +4258,7 @@ AvailableHacks ={
 					return "Door"
 				else
 					local worldSize = GetAbsoluteWorldSize(object)
-					if (((worldSize.X >= 9 and worldSize.Z <=9) or (worldSize.X <= 9 and worldSize.Z >= 9)) or worldSize.Y > 8) 
+					if (((worldSize.X >= 9 and worldSize.Z <=9) or (worldSize.X <= 9 and worldSize.Z >= 9)) or worldSize.Y > 18) 
 						and (worldSize.Y >= 2 or worldSize.X < 7 or worldSize.Z < 7)
 						and (object:GetAttribute("OrgTrans") or object.Transparency) < .1 then
 						return "Wall"

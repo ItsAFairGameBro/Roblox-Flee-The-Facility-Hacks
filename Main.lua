6d892855701bd7b66ca0fbb3433b4604dd11e3b3
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-03-30 17:04:28 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3464,6 +3464,7 @@ C.AvailableHacks ={
 				local newValue = C.enHacks.Render_HackComputers
 
 				local canBeActive = newValue == true
+				print(tag,canBeActive,(camera.CameraType==Enum.CameraType.Custom or camera.CameraType==Enum.CameraType.Track),isInGame)
 				tag.Enabled=canBeActive and (camera.CameraType==Enum.CameraType.Custom or camera.CameraType==Enum.CameraType.Track) and isInGame
 			end,
 			["ActivateFunction"]=function(newValue)

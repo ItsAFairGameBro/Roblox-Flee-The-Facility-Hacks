22cbc58902d167be836a358d4436fa1eaa583f0c
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-04-04 14:09:22 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -10951,7 +10951,7 @@ C.CommandFunctions = {
 			end
 			local isR6 = targetHuman.RigType == Enum.HumanoidRigType.R6
 			local oldHuman = targetHuman
-			local newHuman = (isR6) and Instance.new("Humanoid") or oldHuman:Clone()----oldHuman:Clone()
+			local newHuman = (isR6 or not isSame) and Instance.new("Humanoid") or oldHuman:Clone()----oldHuman:Clone()
 			newHuman.Name = "FakeHuman"
 			newHuman.Parent = targetChar
 			newHuman:AddTag("RemoveOnDestroy")

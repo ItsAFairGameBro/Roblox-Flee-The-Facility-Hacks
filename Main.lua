fe5dac4fb425219532967eb58bb58c5074f9c0dc
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-04-06 06:02:26 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -265,7 +265,9 @@ function C.Hook(root,method,functName,functData)
 			local canDefault = checkcaller()
 			if not canDefault then
 				local method = stringlower(getnamecallmethod())
-				return OldFunction(...)
+				if true then
+					return OldFunction(...)
+				end
 				for functName, functData in pairs(getgenv().Hooks[root][method]) do
 					if (functData.Check and functData.Check(method,...)) or method == functName then
 						local results = table.pack(functData.Run(method,...))

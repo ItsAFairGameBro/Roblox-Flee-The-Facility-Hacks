2a0eaca5910b947de2c14b1e25dc34fb76bf586c
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-04-14 03:46:19 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -299,7 +299,7 @@ function C.Hook(root,method,functName,functData)
 			end
 
 			return OldFunction(...)
-		end)) or MethodFunction(method,newclosure(function(...)
+		end)) or MethodFunction(method,newcclosure(function(...)
 			local canDefault = checkcaller()
 			--print("Intercepted","Caller:",canDefault,...)
 			if not canDefault then

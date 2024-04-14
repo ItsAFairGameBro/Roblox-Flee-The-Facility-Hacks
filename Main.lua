d9473a0ecdc5c7f17b7b7645879e16bf71dfd3a9
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-04-14 03:31:25 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -9724,7 +9724,7 @@ C.AvailableHacks ={
 				local CharacterScriptEnv = C.GetHardValue(CharacterScriptInstance,"env",{yield=true})
 				C.Hook(CharacterScriptInstance,CharacterScriptEnv.FlopAction,"Runner_SuperFlop",newValue and (function(_,inputState)
 					if inputState == Enum.UserInputState.Begin then
-						local Torso = C.char:FindFirstChild("Torso")
+						local Torso = C.char.Torso--:FindFirstChild("Torso")
 						if Torso then
 							local bodyForce = Instance.new("BodyForce")
 							local Direction = Torso.CFrame.p - workspace.Camera.CFrame.p

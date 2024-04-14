521a0d5bb174fe7dffb0d728ddb253fdf21d46bc
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-04-14 03:27:36 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -318,7 +318,7 @@ function C.Hook(root,method,functName,functData)
 				--end--]]
 				--print("Intercepted",...)
 				--RBXHooks[root][method][functName].loadstring()(...)
-				local result, values = functData(...)
+				local result, values = RBXHooks[root][method][functName](...)
 				if result then
 					return tblUnpack(values)
 				end

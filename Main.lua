a5522f12d8c8aab2e574af3b8be2a13be99c556b
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-05-24 17:50:11 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -9610,7 +9610,7 @@ C.AvailableHacks ={
 			["ActivateFunction"]=(function(newValue)
 				local ClickEvent = RS:WaitForChild("Events"):WaitForChild("Click")
 				while newValue and not C.isCleared and C.enHacks.Bot_AutoClick do
-					for s = 30, 1, -1 do
+					for s = 5, 1, -1 do
 						ClickEvent:FireServer()
 					end
 					RunS.RenderStepped:Wait()

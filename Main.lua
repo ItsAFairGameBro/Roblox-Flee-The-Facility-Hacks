f1c2b0c062cdb1293ea976f33459b2271cc69f85
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-05-29 21:01:10 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -5226,7 +5226,8 @@ C.AvailableHacks ={
 						error("Purposeful Error!")
 					end
 					local RunScript = getcallingscript()
-					traceback(`22The script has successfully intercepted an attempted kick from: {RunScript:GetFullName()} {RunScript.Parent or "nilpar"} and Disabled={RunScript.Disabled}`)
+					
+					print(traceback(`22The script has successfully intercepted an attempted kick from: {RunScript:GetFullName()} {RunScript.Parent or "nilpar"} and Disabled={RunScript.Disabled}`))
 					if C.gameUniverse == "FlagWars" then
 						if RunScript.Name=="BAC_" then
 							if C.BAC ~= RunScript then

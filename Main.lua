fcfc31e46aea2965e483f26e652163cb3a8d0281
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-06-07 11:07:31 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1264,7 +1264,7 @@ C.CommandFunctions = {
 	},
 	["time"]={
 		Type="Player",
-		AfterTxt="%.2f",
+		AfterTxt="%.1f",
 		Run=function(args)
 			return true,time()
 		end,
@@ -12046,41 +12046,7 @@ task.spawn(function()
 		return
 	else
 		if true then
-			local OldRemoteEvent
 			
-			OldRemoteEvent = hookfunction(Instance.new("RemoteEvent").FireServer,function(event,...)
-				local arguments = {...}
-				
-				
-				if tostring(getcallingscript()) == "BAC_" then
-					if tostring(event) == "NewMessage" and (arguments[1] == "B-1" or arguments[1] == "A-1") then
-						print("Prevented BAC_ with",event.Name,...)
-						return error("Nope")
-					else
-						print("Passed BAC_ with",event.Name,...)
-					end
-				end
-				
-				return OldRemoteEvent(event,...)
-			end)
-			
-			local OldPreloadAsync
-			local Print2 = true
-			
-			OldPreloadAsync = hookfunction(CP.PreloadAsync,function(CP_2,assets,callerFunction)
-				
-				if tostring(getcallingscript()) == "BAC_" then
-					if Print2 then
-						Print2 = false
-						print("PreloadAsync Instance: "..assets[1]:GetFullName())
-					else
-						print("Blocked BAC_ from calling",#assets)
-					end
-					return
-				end
-				
-				return OldPreloadAsync(CP_2,assets,callerFunction)
-			end)
 			
 			
 			return

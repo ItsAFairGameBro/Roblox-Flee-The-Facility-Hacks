86f1686729c77bdd09544b989c0f19457fb02305
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-06-07 22:49:14 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3991,7 +3991,7 @@ C.AvailableHacks ={
 						continue
 					end
 					if connection.Function ~= C.BetterConsole_onErrorOut and newValue then
-						warn("FunctDiff:",connection.Function,C.BetterConsole_onErrorOut)
+						warn("FunctDiff:",connection.Thread,C.BetterConsole_onErrorOut)
 						if connection.Enabled then
 							connection:Disable()
 							warn("Disabled "..num.."/"..#Connections)

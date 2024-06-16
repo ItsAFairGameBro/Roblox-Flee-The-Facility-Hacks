8f6527f6c8e43be90843125f712734b61f7e7483
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-06-15 19:48:07 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -70,7 +70,7 @@ local reloadFunction = lastRunningEnv.ReloadFunction--function()
 local GlobalSettings = lastRunningEnv.GlobalSettings or {}
 local isTeleportingAllowed = GlobalSettings.isTeleportingAllowed~=false
 GlobalSettings.MinimumHeight = GlobalSettings.MinimumHeight or 1.5
-GlobalSettings.BetterConsole = true
+GlobalSettings.BetterConsole = false
 
 local getID="HackGUI1"
 local emojiDesc = {

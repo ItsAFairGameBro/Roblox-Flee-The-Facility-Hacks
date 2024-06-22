d63cf86b228c5c81d5ac266cdfa0cc07a19c59ce
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-06-22 08:06:47 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3363,7 +3363,7 @@ C.AvailableHacks ={
 						local SpeedMult = (VehicleType=="Plane" and C.enHacks.Blatant_NavalVehicleSpeed or math.min(1.8,C.enHacks.Blatant_NavalVehicleSpeed))
 						lastSet = SpeedMult * LineVelocity.VectorVelocity
 						LineVelocity.VectorVelocity = lastSet
-						AlignOrientation.Reponsiveness = 20 * SpeedMult
+						AlignOrientation.Responsiveness = 20 * SpeedMult
 					end
 					C.AvailableHacks.Blatant[320].Funct = LineVelocity:GetPropertyChangedSignal("VectorVelocity"):Connect(Upd)
 					Upd()

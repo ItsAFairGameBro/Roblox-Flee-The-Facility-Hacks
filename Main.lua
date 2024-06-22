76154a3bb5127a134149b5133655fe3de33bb610
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-06-22 09:41:16 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3365,13 +3365,15 @@ C.AvailableHacks ={
 						local SpeedMult = (VehicleType=="Plane" and C.enHacks.Blatant_NavalVehicleSpeed or math.min(1.8,C.enHacks.Blatant_NavalVehicleSpeed))
 						local TurnMult = C.enHacks.Blatant_NavalVehicleTurnSpeed
 						lastSet = SpeedMult * LineVelocity.VectorVelocity
-						if C.enHacks.Blatant_NavalInfPlaneFuel then
-							if FuelLeft.Value < 500 then
-								FuelLeft:SetAttribute("RealFuel",FuelLeft.Value)
+						if FuelLeft then
+							if C.enHacks.Blatant_NavalInfPlaneFuel then
+								if FuelLeft.Value < 500 then
+									FuelLeft:SetAttribute("RealFuel",FuelLeft.Value)
+								end
+								FuelLeft.Value = math.huge
+							elseif FuelLeft:GetAttribute("RealFuel") then
+								FuelLeft.Value = FuelLeft:GetAttribute("RealFuel")
 							end
-							FuelLeft.Value = math.huge
-						elseif FuelLeft:GetAttribute("RealFuel") then
-							FuelLeft.Value = FuelLeft:GetAttribute("RealFuel")
 						end
 						local isOn = (LineVelocity.MaxForce > 10 and (not FuelLeft or (FuelLeft:GetAttribute("RealFuel") or FuelLeft.Value) > 0)) or 
 							(FlyButton.BackgroundColor3.R*255>250 and C.enHacks.Blatant_NavalInfPlaneFuel)

2116a264ff8d919f2510c57f7886c9cf29524499
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-06-29 16:25:58 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2648,7 +2648,7 @@ C.AvailableHacks ={
 							if not C.GetAction("Plane Refuel") and BombC.Value > 0 then
 								PlaneMB.AssemblyLinearVelocity = TargetCF.Position - PlaneMB.Position
 								PlaneMB.AssemblyAngularVelocity = Vector3.new()
-								if BombC.Value > 0 and WhileIn>.2 then
+								if BombC.Value > 0 and WhileIn>1/3 then
 									WhileIn = 0
 									C.RemoteEvent:FireServer("bomb")
 								end
